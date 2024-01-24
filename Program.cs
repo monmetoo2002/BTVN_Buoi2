a538dd8bdfe3f5869216310b867497d3d3f31b25
quyphung994 <quypn994@gmail.com>
2024-01-24 20:48:23 +0700

    Hoan thanh bai tap

--- Program.cs
+++ Program.cs
@@ -5,23 +5,7 @@ class Buoi2
 {
     public static void Main()
     {
-        int[] test1 = { 1, 2, 3, 3, 1};
-        int[] test2 = { 1, 1, 1, 1, 1, 6};
-        int[] test3 = { 1, 2, 3, 4, 3, 2, 1};
-        int[] test4 = { 1, 3, 3, 3, 1, 2, 5, 2};
-
-        string[] name = { "A", "B", "C", "D", "E", "F", "G", "H"};
-        int[] grade = { 5, 4, 5, 7, 4, 6, 7, 7};
-
-
-        System.Console.WriteLine("BTVN Buoi 2:");
-        System.Console.WriteLine("Ket qua bai 1:");
-        System.Console.WriteLine("test case 1: " + Bai1(test1));
-        System.Console.WriteLine("test case 2: " + Bai1(test2));
-        System.Console.WriteLine("test case 3: " + Bai1(test3));
-        System.Console.WriteLine("test case 4: " + Bai1(test4));
-        System.Console.WriteLine("Ket qua bai 2:");
-        Bai2(name, grade);
+        //ABC
     }
 
     private static string Bai1(int[] input)
